d7131b60e5809caf6e169d7ebb551ef8bc259b5b
Rafael Willen <rafaelpadre20@gmail.com>
2025-03-08 13:59:26 +0100

    feat: implement mobile layout for the introduction section

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -9,19 +9,27 @@ type UI = Record<keyof typeof languages, Record<string, string>>;
 
 export const ui: UI = {
   en: {
+    // Navbar
     "nav.home": "Home",
     "nav.services": "Services",
     "nav.portfolio": "Portfolio",
     "nav.testimonials": "Testimonials",
     "nav.language": "Language",
     "nav.contact": "Contact Us",
+    // Introduction section
   },
   pt: {
+    // Navbar
     "nav.home": "Início",
     "nav.services": "Serviços",
     "nav.portfolio": "Portfólio",
     "nav.testimonials": "Depoimentos",
     "nav.language": "Idioma",
     "nav.contact": "Contacto",
+    // Introduction section
+    "introduction.title":
+      "Bold Labs: Transformando Narrativas, Liderando a Inovação em África",
+    "introduction.subtitle":
+      "Construímos pontes entre a tradição e o futuro, capacitando marcas africanas a brilharem globalmente através de criatividade e inovação.",
   },
 } as const;
