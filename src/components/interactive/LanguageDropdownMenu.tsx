fcbb6bdac465244085c06bba6f3d135bf9b3fc2a
Rafael Willen <rafaelpadre20@gmail.com>
2025-03-08 13:20:44 +0100

    feat: add internationalization for the navbar

--- src/components/interactive/LanguageDropdownMenu.tsx
+++ src/components/interactive/LanguageDropdownMenu.tsx
@@ -6,20 +6,26 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { ChevronDown } from "lucide-react";
 
-export default function LanguageDropdownMenu() {
+type Props = {
+  label: string;
+  languages: {
+    code: string;
+    label: string;
+  }[];
+};
+export default function LanguageDropdownMenu({ label, languages }: Props) {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger className="flex items-center gap-2.5">
-        Idioma
+        {label}
         <ChevronDown />
       </DropdownMenuTrigger>
       <DropdownMenuContent className="bg-txt-100 text-white border-txt-200 px-4">
-        <DropdownMenuItem>
-          <a href="#pt">Português</a>
-        </DropdownMenuItem>
-        <DropdownMenuItem>
-          <a href="#en">English</a>
-        </DropdownMenuItem>
+        {languages.map(({ code, label }) => (
+          <DropdownMenuItem key={code}>
+            <a href={`/${code}`}>{label}</a>
+          </DropdownMenuItem>
+        ))}
       </DropdownMenuContent>
     </DropdownMenu>
   );
--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -8,6 +8,20 @@ export const defaultLang = "pt";
 type UI = Record<keyof typeof languages, Record<string, string>>;
 
 export const ui: UI = {
-  en: {},
-  pt: {},
+  en: {
+    "nav.home": "Home",
+    "nav.services": "Services",
+    "nav.portfolio": "Portfolio",
+    "nav.testimonials": "Testimonials",
+    "nav.language": "Language",
+    "nav.contact": "Contact Us",
+  },
+  pt: {
+    "nav.home": "Início",
+    "nav.services": "Serviços",
+    "nav.portfolio": "Portfólio",
+    "nav.testimonials": "Depoimentos",
+    "nav.language": "Idioma",
+    "nav.contact": "Contacto",
+  },
 } as const;
