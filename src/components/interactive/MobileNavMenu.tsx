c7ecb5d166f7c3d18503cb61f6738a7ce9351cc3
Rafael Willen <rafaelpadre20@gmail.com>
2025-03-30 14:41:53 +0100

    feat: refactor the component

--- src/components/interactive/MobileNavMenu.tsx
+++ src/components/interactive/MobileNavMenu.tsx
@@ -1,68 +1,41 @@
-import { useRef, useState, type PropsWithChildren } from "react";
+import { Menu, X } from "lucide-react";
+import { useState, type PropsWithChildren } from "react";
 
 export default function MobileNavMenu({ children }: PropsWithChildren) {
   const [isOpen, setIsOpen] = useState(false);
-  const overlayElementRef = useRef<HTMLDivElement>(null);
 
-  const openMenuClick = () => {
+  const openMenuOnClick = () => {
     setIsOpen(true);
-    overlayElementRef.current?.style.setProperty("width", "100%");
     document.body.style.overflow = "hidden";
   };
 
-  const closeMenuClick = () => {
+  const closeMenuOnClick = () => {
     setIsOpen(false);
-    overlayElementRef.current?.style.setProperty("width", "0");
     document.body.style.overflow = "auto";
   };
 
   return (
     <div className="relative block lg:hidden">
       <button
-        onClick={openMenuClick}
+        onClick={openMenuOnClick}
         type="button"
         className="bg-primary flex size-10 items-center justify-center rounded-lg md:size-14"
       >
-        <svg
-          className="size-6 md:size-8"
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M3 12H21"
-            stroke="white"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          ></path>
-          <path
-            d="M3 6H21"
-            stroke="white"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          ></path>
-          <path
-            d="M3 18H21"
-            stroke="white"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          ></path>
-        </svg>
+        <Menu className="text-white" />
       </button>
       {/* Overlay */}
       <div
-        ref={overlayElementRef}
+        style={{
+          width: isOpen ? "100%" : "0",
+        }}
         className="fixed top-0 left-0 z-10 h-full w-0 overflow-x-hidden bg-black/90 duration-300"
       >
         <button
           className="absolute top-5 right-11 text-6xl text-white"
           type="button"
-          onClick={closeMenuClick}
+          onClick={closeMenuOnClick}
         >
-          &times;
+          <X />
         </button>
         {/* Overlay content */}
         <nav className="relative top-1/4 mt-7 w-full text-center">
