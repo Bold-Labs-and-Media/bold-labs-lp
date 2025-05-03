6df4b84b2366de017ac44639c95937b386154ecd
Rafael Willen <rafaelpadre20@gmail.com>
2025-05-03 12:38:17 +0100

    feat: add more services

--- src/components/interactive/Services.tsx
+++ src/components/interactive/Services.tsx
@@ -1,80 +1,117 @@
+import { services } from "@/lib/services-data";
 import { cn, useTranslations } from "@/lib/utils";
-import { ArrowRight } from "lucide-react";
+import { ArrowLeft, ArrowRight } from "lucide-react";
 import { useCallback, useState } from "react";
 
 type Props = {
   lang: "en" | "pt";
 };
 
-const DUMMY_IMAGES = [
-  "https://images.unsplash.com/photo-1507461476191-0ed4f9f18533?q=80&w=640&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  "https://images.unsplash.com/photo-1519635451045-a41d4361d495?q=80&w=640&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  "https://images.unsplash.com/photo-1611693196679-c1f358840cc6?q=80&w=640&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-];
-
 export default function Services({ lang }: Props) {
-  const t = useCallback(useTranslations(lang), []);
-  const [selectedIndex, setSelectedIndex] = useState(0);
-  const services = [t("services.1"), t("services.2"), t("services.3")];
+  const servicesIds = Array.from({ length: 6 }).map(
+    (_, index) => `services.${index + 1}`,
+  );
+  const servicesIdFirstHalf = servicesIds.slice(0, 3);
+  const servicesIdSecondHalf = servicesIds.slice(3);
+  const [selectedService, setSelectedService] = useState(servicesIds[0]);
+  console.log(selectedService);
 
   return (
-    <>
-      <div className="row-span-2 mt-6 md:mt-0">
-        <p className="text-xxs animation-left-title-appear text-center leading-[170%] md:text-left md:text-xl">
-          {t("services.subtitle")}
-        </p>
-        <ul className="mt-8.5 flex flex-col gap-8">
-          {services.map((service, index) => (
-            <li key={index}>
-              <button
-                style={{
-                  animationDelay: `${index * 0.2}s`,
-                  animationRange: "entry 0% cover 15%",
-                }}
-                type="button"
-                onClick={() => setSelectedIndex(index)}
-                className={cn(
-                  "hover:bg-txt-100 hover:text-txt-400 animation-left-title-appear rounded-5xl border-txt-400 text-foreground flex w-full cursor-pointer items-center gap-3 border px-10 py-8 text-sm font-medium duration-500 hover:shadow-2xl md:text-xl",
-                  index == selectedIndex && "bg-txt-100 text-txt-400",
-                )}
-              >
-                <span
-                  className={cn(
-                    "font-monument text-lg",
-                    index === selectedIndex && "text-primary",
-                  )}
-                >
-                  0{index + 1}
-                </span>
-                <p className="flex-1">{service}</p>
-                <ArrowRight className="hidden md:block" size={24} />
-              </button>
-            </li>
-          ))}
-        </ul>
+    <div className="mt-10 grid grid-cols-1 gap-x-4 md:grid-cols-3">
+      <div className="md:mt-0">
+        <ServiceList
+          lang={lang}
+          isFirstHalf
+          services={servicesIdFirstHalf}
+          onServiceSelect={setSelectedService}
+          selectedService={selectedService}
+        />
       </div>
-      <div className="row-span-2 hidden h-full md:block">
-        {DUMMY_IMAGES.map((image, index) => (
+      <div className="hidden h-full md:block">
+        {services.map(({ id, imgUrl }) => (
           <img
-            key={index}
-            src={image}
+            key={id}
+            height={580}
+            src={imgUrl}
             alt=""
             style={
-              index !== selectedIndex
+              id !== selectedService
                 ? {}
-                : {
-                    animationRange: "entry 0% cover 40%",
-                  }
+                : { animationRange: "entry 0% cover 40%" }
             }
             className={cn(
-              "rounded-2.5xl h-full object-cover object-center",
-              index !== selectedIndex
+              "rounded-2.5xl h-[580px] object-cover object-center shadow-2xl",
+              id !== selectedService
                 ? "hidden"
                 : "animation-appear animate-delay-[200ms]",
             )}
           />
         ))}
       </div>
-    </>
+      <div className="md:mt-0">
+        <ServiceList
+          lang={lang}
+          services={servicesIdSecondHalf}
+          onServiceSelect={setSelectedService}
+          selectedService={selectedService}
+        />
+      </div>
+    </div>
   );
 }
+
+type ServiceProps = {
+  services: string[];
+  onServiceSelect: (service: string) => void;
+  selectedService: string;
+  isFirstHalf?: boolean;
+  lang: "en" | "pt";
+};
+
+const ServiceList = ({
+  services,
+  onServiceSelect,
+  selectedService,
+  isFirstHalf = false,
+  lang,
+}: ServiceProps) => {
+  const t = useCallback(useTranslations(lang), []);
+  return (
+    <ul className="flex flex-col gap-8">
+      {services.map((service, index) => (
+        <li key={service}>
+          <button
+            style={{
+              animationDelay: `${index * 0.2}s`,
+              animationRange: "entry 0% cover 15%",
+            }}
+            type="button"
+            onClick={() => onServiceSelect(service)}
+            className={cn(
+              "hover:bg-txt-100 hover:text-txt-400 animation-left-title-appear rounded-5xl border-txt-400 text-foreground flex w-full cursor-pointer items-center gap-3 border px-10 py-8 text-sm font-medium duration-500 hover:shadow-2xl md:text-xl",
+              service == selectedService && "bg-txt-100 text-txt-400",
+            )}
+          >
+            <span
+              className={cn(
+                "font-monument text-lg",
+                service === selectedService && "text-primary",
+                !isFirstHalf && "order-2",
+              )}
+            >
+              0{index + (isFirstHalf ? 1 : 4)}
+            </span>
+            <p className={cn("flex-1", !isFirstHalf && "order-2")}>
+              {t(service)}
+            </p>
+            {isFirstHalf ? (
+              <ArrowRight className="hidden md:block" size={24} />
+            ) : (
+              <ArrowLeft className="order-1 hidden md:block" size={24} />
+            )}
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
